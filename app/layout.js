794e9af8b92700c1a4f504900cded4041ce0c6c3
Jaiden Ratti <jaidenratti@gmail.com>
2025-01-05 23:13:04 -0500

    analytics

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import "./globals.css";
 import SessionProviderWrapper from "./SessionProviderWrapper";
+import { Analytics } from "@vercel/analytics/react"
+
 
 export const metadata = {
   title: "Strava Activity Graph",
@@ -10,7 +12,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <SessionProviderWrapper>{children}</SessionProviderWrapper>
+        <SessionProviderWrapper>{children}
+        <Analytics/>
+        </SessionProviderWrapper>
       </body>
     </html>
   );  
